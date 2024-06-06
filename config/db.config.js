ed5f874fd9fb0104b39eac4155f3eab2d7e81feb
westjuuu <westjiuuu@naver.com>
2024-06-06 21:47:56 +0900

    Update db.config.js to improve database connection handling

--- config/db.config.js
+++ config/db.config.js
@@ -4,13 +4,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    port: process.env.DB_PORT || 3306,
-    database: process.env.DB_DATABASE || 'workbook', 
-    password: process.env.DB_PASSWORD || 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE, 
+    password: process.env.DB_PASSWORD,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-});
+}); 
+async function checkDatabaseConnection() {
+    try {
+        const connection = await pool.getConnection();
+        console.log('데이터베이스 연결 성공!');
+        connection.release();  // 연결 성공 후, 연결을 풀에 반환
+    } catch (error) {
+        console.error('데이터베이스 연결 실패:', error);
+    }
+}
+
+checkDatabaseConnection();
