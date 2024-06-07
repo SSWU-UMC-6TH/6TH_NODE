7e00abeb57f7a4d90aa150b1e2fd8a812f60ac36
hyen <hi020514@gmail.com>
2024-06-07 22:00:06 +0900

    fix:9주차

--- config/swagger.config.js
+++ config/swagger.config.js
@@ -5,7 +5,7 @@ import SwaggerJsdoc from "swagger-jsdoc";
 const options = {
     definition: {
         info: {
-            title: 'UMC Study API',
+            title: 'May Study API',
             version: '1.0.0',
             description: 'UMC Study API with express, API 설명'
         },
